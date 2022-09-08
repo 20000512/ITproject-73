85d93d7eddc5bbac89c5a586ebb3563d55147127
Wai Kin Wilkin Chow <waikinwilkin.chow@student.unimelb.edu.au>
2022-09-08 15:14:18 +0800

    login chang and add first page

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,10 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://wilkin:tryone@cluster0.0q0ztzg.mongodb.net/test?retryWrites=true&w=majority')
+app.get('/', async (req, res) => {
+	res.send("First page use /api/register to add new user use /api/login for login")
 
+})
 app.post('/api/register', async (req, res) => {
 	console.log(req.body)
 	try {
@@ -35,64 +38,15 @@ app.post('/api/register', async (req, res) => {
 app.post('/api/login', async (req, res) => {
 	const user = await User.findOne({
 		email: req.body.email,
+		password: req.body.password,
 	})
-
-	if (!user) {
-		return { status: 'error', error: 'Invalid login' }
-	}
-
-	const isPasswordValid = await bcrypt.compare(
-		req.body.password,
-		user.password
-	)
-
-	if (isPasswordValid) {
-		const token = jwt.sign(
-			{
-				name: user.name,
-				email: user.email,
-			},
-			'secret123'
-		)
-
-		return res.json({ status: 'ok', user: token })
-	} else {
-		return res.json({ status: 'error', user: false })
+	if (user) {
+		return res.json({ status: 'ok', user: 'true' })
+	}else {
+		return res.json ({ status: 'error', user: 'false' })
 	}
 })
-
-app.get('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
-
-	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		const user = await User.findOne({ email: email })
-
-		return res.json({ status: 'ok', quote: user.quote })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
-	}
-})
-
-app.post('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
-
-	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		await User.updateOne(
-			{ email: email },
-			{ $set: { quote: req.body.quote } }
-		)
-
-		return res.json({ status: 'ok' })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
-	}
-})
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 app.listen(1337, () => {
 	console.log('Server started on 1337')
 })
