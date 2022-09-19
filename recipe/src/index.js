be68e09ad881b0b3fe4c6e09688e3d48feadb664
Ziqian <89445781+Ziqian2233@users.noreply.github.com>
2022-09-19 13:11:38 +0800

    change 5

--- recipe/src/index.js
+++ recipe/src/index.js
@@ -8,8 +8,18 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 import Home from './pages/home';
-import Login from './pages/login';
+import Login from './pages/auth/login';
+import Search from './pages/search';
+import Sort from './pages/sort';
+import Profile from './pages/profile';
+import Signin from './pages/auth/signin';
+import Forget from './pages/auth/forget';
+import Detail from './pages/detail';
+import Setting from './pages/profile/setting';
+import ChangePassword from './pages/profile/changepassword';
+import Personal from './pages/profile/personal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,9 +28,19 @@ root.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/sort" element={<Sort />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
+        <Route path="personal" element={<Personal />} />
+        <Route path="changepassword" element={<ChangePassword />} />
+        <Route path="setting" element={<Setting />} />
+        <Route path="detail/:id" element={<Detail />} />
+        <Route path="forget" element={<Forget />} />
         <Route path="login" element={<Login />} />
+        <Route path="signin" element={<Signin />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   </React.StrictMode>
 );
