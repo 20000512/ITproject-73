c4b70fbc52915116b8bf10efad2f419c2b374b8b
Ziqian <89445781+Ziqian2233@users.noreply.github.com>
2022-10-07 22:26:27 +0800

    add username

--- recipe/src/pages/auth/signin.js
+++ recipe/src/pages/auth/signin.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import { useNavigate } from 'react-router-dom';
 import PageWrapper from '../../components/pagewrapper';
 import toast from 'react-hot-toast';
@@ -11,10 +12,12 @@ const Signin = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
   const [repassword, setRepassword] = useState('');
 
   const handleClick = () => {
-    if (!username) return toast.error('Email cannot be blank');
+    if (!email) return toast.error('Email cannot be blank');
+    if (!username) return toast.error('Username cannot be blank');
     if (!password) return toast.error('Password cannot be blank');
     if (!repassword) return toast.error('RePassword cannot be blank');
     if(repassword !== password) return toast.error('RePassword is not the same as the password');
@@ -42,7 +45,7 @@ const Signin = () => {
             }}>
               <AccountCircleOutlinedIcon sx={{ fontSize: '60px' }} />
               <Typography variant="h4" color="inherit">
-                {"Sign up"}
+                {"Sign in"}
               </Typography>
             </Box>
             <Box sx={{
@@ -51,7 +54,27 @@ const Signin = () => {
               <TextField
                 sx={{ width: '400px' }}
                 id="outlined-name"
-                label="Emial address"
+                label="Email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                margin="normal"
+                variant="outlined"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <EmailOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+            <Box sx={{
+              display: 'flex', justifyContent: 'center'
+            }}>
+              <TextField
+                sx={{ width: '400px' }}
+                id="outlined-name"
+                label="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 margin="normal"
@@ -59,7 +82,7 @@ const Signin = () => {
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
-                      <EmailOutlinedIcon />
+                      <PersonOutlinedIcon />
                     </InputAdornment>
                   ),
                 }}
@@ -111,11 +134,11 @@ const Signin = () => {
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'end', }}>
               <Typography onClick={() => navigate('/login')} sx={{ textDecoration: 'underline', cursor: 'pointer' }} variant="body" color="#787878">
-                Back to log in
+                back to log in
               </Typography>
             </Box>
             <Button onClick={() => handleClick()} sx={{ marginTop: '30px', background: '#ffa65c', color: 'white' }} variant="outlined" color="warning">
-              {"Sign up"}
+              {"Sign in"}
             </Button>
           </Box>
         </form>
