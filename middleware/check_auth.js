872a4061220d483e38b320ab3c584a151f291a17
Wilkin Chow <wilkin908@gmail.com>
2022-10-18 19:40:30 +0800

    fail auth

--- middleware/check_auth.js
+++ middleware/check_auth.js
@@ -5,6 +5,7 @@ JWT_SECRET = 'asdasdasdasdsdf+659+523ewrfgarf6r5faw+f+-**/-/-*/*5*/3-*5/3-*5/266
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
         const decoded = jwt.verify(token, JWT_SECRET);
         
         // Token verification successful
@@ -12,6 +13,6 @@ module.exports = (req, res, next) => {
         next();
     } catch (err) {
         // Token verification failed
-        return res.status(401).json('Auth failed');
+        return res.status(401).json('Aasdasdailed');
     }
 };
