06a3de01b58e56e859a1cd8b0dfaf18c9dcc0796
Wilkin Chow <wilkin908@gmail.com>
2022-11-06 15:29:15 +0800

    category finish

--- frontend/src/pages/category.js
+++ frontend/src/pages/category.js
@@ -2,14 +2,21 @@ import { Box, Tabs, Tab, Typography } from '@mui/material';
 import PageWrapper from '../components/pagewrapper';
 import NavBarWrapper from '../components/navbarwrapper';
 import Navpagewrapper from '../components/navpagewrapper';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {host} from './host';
+import axios from 'axios';
+import ItemCard from '../components/itemcard';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 const Category= () => {
     const navigate = useNavigate();
+    const [keywords, setKeywords] = useState('');
+    const [results, setResults] = useState([]);
+    const [show, setShow] = useState(false);
     const [data, setData] = useState([{
       id: 1,
       type: 'main',
-      list: ['food', 'food', 'food', 'food', 'food', 'food']
+      list: ['hi', 'food', 'food', 'food', 'food', 'food']
     }, {
       id: 2,
       type: 'other',
@@ -19,20 +26,47 @@ const Category= () => {
       type: 'food',
       list: ['food food', 'food food', 'food food', 'food food']
     }])
+    useEffect(() => {
+      console.log(keywords)
+      const expensesListResp = async () => {
+        if (keywords) {
+          // Keyword not empty, get search results
+          await axios.get(host + '/recipes/search/' + keywords)
+            .then(response => {
+                // Store search results
+                setResults(response.data.data);
+                // Show search results
+                setShow(true);
+            })
+        }
+      } 
+      expensesListResp();
+    }, [keywords]);
   
     const [active, setActive] = useState(data[0]);
     return (
       <PageWrapper>
         <NavBarWrapper>
-          <Box sx={{ width:'100%', height: '64px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Typography variant='h5'>Category</Typography>
-          </Box>
+          <Box sx={{ width: '100%', height: '64px', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative' }}>
+          {show ? <ArrowBackIosIcon
+            sx={{ cursor: "pointer", position: "absolute", left: "30px" }}
+            onClick={() => {setShow(false);setKeywords('')}}
+          /> : ''}
+          <Typography variant='h5'>Category</Typography>
+        </Box>
         </NavBarWrapper>
         <Navpagewrapper>
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: '30px' }}>
-          <Box
-        sx={{ flexGrow: 1, width: '100%', display: 'flex' }}
-      >
+          {show ? <Box sx={{ width: '100%', maxWidth: '1000px' }}>
+            {results.map(e => <ItemCard
+              onClick={() => navigate('/detail/' + e._id)}
+              sx={{ mb: '30px' }}
+              key={e._id}
+              title={e.title}
+              cover={e.cover}
+              description={e.description}
+            />)}
+          </Box> : <Box sx={{ flexGrow: 1, width: '100%', display: 'flex' }}>
           <Tabs
           orientation="vertical"
           variant="scrollable"
@@ -40,14 +74,15 @@ const Category= () => {
           onChange={() => console.log('Active')}
           aria-label="Vertical tabs example"
           sx={{ borderRight: 1, borderColor: 'divider' }}
-        >
+          >
           {data.map((e, index) => <Tab key={index} label={e.type} sx={{
             backgroundColor: active.id === e.id ? '#f57c18' : 'transparent'
           }} onClick={() => setActive(e)} />)}
         </Tabs>
-        <Box sx={{ display: 'flex'}}>{active.list.map((ele, index) => <Typography sx={{ px: '16px', py: '8px'}} onClick={() => navigate('/search')} key={index}>{ele}</Typography>)}</Box>
-        </Box>
+        <Box sx={{ display: 'flex'}}>{active.list.map((ele, index) => <Typography sx={{ px: '16px', py: '8px'}} onClick={() => setKeywords(ele)} key={index}>{ele}</Typography>)}</Box>
+        </Box>}
           </Box>
+        
         </Navpagewrapper>
       </PageWrapper>
     );
--- routes/users.test.js
+++ routes/users.test.js
@@ -1,11 +1,10 @@
 const users = require ("./users.js")
 const supertest = require('supertest')
 const router = require('express').Router();
-const request = require('supertest')
+const request = require('supertest');
+const { response } = require("express");
 test("should respond with a 200 tatus code",async () => {
-    console.log("aaaaaaaaaaaaaaaaa");
     const response = await request(users).post("/signup").send({"email":"wwww12dSD3","password":"adasdasdas","username":"adasdasdasdaYrrrrY"})
-    console.log("BBBBBBBBBBBBB");
     expect(response.statusCode).toBe(200)
 })
         
