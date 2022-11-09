0fbd8a1a0423356e383602a178bba5eb4c52ed75
Wilkin Chow <wilkin908@gmail.com>
2022-11-09 17:04:10 +0800

    add recipes.test

---
+++ routes/recipes.test.js
@@ -0,0 +1,10 @@
+const host = 'http://localhost:5003'
+const supertest = require('supertest')
+const request = require('supertest');
+const server = request.agent(host);
+describe('recipes',  ()=> {
+    test('get hottest recipes', async () => {
+      const data = await server.get("/recipes/hot?page=int&limit=int") 
+      expect(data.statusCode).toBe(200);
+    });
+  })
--- routes/users.test.js
+++ routes/users.test.js
@@ -1,6 +1,7 @@
+const host = 'http://localhost:5003'
 const supertest = require('supertest')
 const request = require('supertest');
-const server = request.agent("http://localhost:5003");
+const server = request.agent(host);
 describe('users',  ()=> {
   test('sigin up sucessful', async () => {
     const data = await server.post("/users/signup").send({"email":"wwww12dSD3dasdaaa","password":"adasdasdas","username":"adasdasdasdaYrrrrY"})
@@ -12,7 +13,6 @@ describe('users',  ()=> {
   });
   test('delete', async () => {
     const token = await (await server.post("/users/login").send({"email":"wwww12dSD3dasdaaa","password":"adasdasdas"})).body.token
-    console.log(token)
     const data = await server.put("/users/delete").send({}).set({'authorization' : `Bearer ${token}`})
     expect(data.statusCode).toBe(200)
   });
